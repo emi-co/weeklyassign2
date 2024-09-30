d4934b8caef07d8f35014256838c4eb270aae1a9
emi-co <200~158002903+emi-co@users.noreply.github.com>
2024-09-30 11:58:09 -0400

    Second Additions

--- js.js
+++ js.js
@@ -1,1 +1,22 @@
 let hands = ["rock", "paper", "scissors"];
+let com_hands;
+
+function game(p_hands) {
+  com_hands = Math.floor(Math.random() * 3);
+
+  if (com_hands === p_hands) {
+    return "Draw";
+    
+  } else if (com_hands === 0 && p_hands === 1 || 2) {
+    return "You lose";
+  }
+  
+  } else if (com_hands === 1 || 2 && p_hands === 0) {
+    return "You win";
+  
+  } else if (com_hands === 2 && p_hands === 1) {
+    return "You lose";
+  
+  } else if (com_hands === 1 && p_hands === 2) {
+  return "You win";
+}
