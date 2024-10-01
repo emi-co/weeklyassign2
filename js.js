5b1e46f15dd77063149625c9e08aa2bfc4f4dbc3
emi-co <200~158002903+emi-co@users.noreply.github.com>
2024-10-01 02:15:03 -0400

    First designs

--- js.js
+++ js.js
@@ -6,17 +6,13 @@ function game(p_hands) {
 
   if (com_hands === p_hands) {
     return "Draw";
-    
-  } else if (com_hands === 0 && p_hands === 1 || 2) {
+  } else if (com_hands === 0 && p_hands === 1) {
     return "You lose";
-  }
-  
-  } else if (com_hands === 1 || 2 && p_hands === 0) {
-    return "You win";
-  
-  } else if (com_hands === 2 && p_hands === 1) {
-    return "You lose";
-  
   } else if (com_hands === 1 && p_hands === 2) {
-  return "You win";
+    return "You lose";
+  } else if (com_hands === 2 && p_hands === 0) {
+    return "You lose";
+  } else {
+    return "You win";
+  }
 }
