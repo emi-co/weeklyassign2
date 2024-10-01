93a84bf099684f7292d75093e6af2d68d834ce25
emi-co <200~158002903+emi-co@users.noreply.github.com>
2024-10-01 05:08:44 -0400

    JS Updates

--- js.js
+++ js.js
@@ -1,18 +1,43 @@
-let hands = ["rock", "paper", "scissors"];
-let com_hands;
+// const prompt = require("prompt-sync")();
+let p_score = 0;
+let com_score = 0;
+let tie = 0;
+const p_choice = document.getElementById("p_choice");
+const com_choice = document.getElementById("com_choice");
+const result_text = document.getElementById("result_text");
+const score = document.getElementById("score");
 
-function game(p_hands) {
+{
+  /* <div id="p_choice">You chose:</div>
+    <div id="com_choice">The computer chose:</div>
+    <div id="result_text">Draw</div> */
+}
+
+function playGame(p_hands) {
   com_hands = Math.floor(Math.random() * 3);
+  let hands = ["rock", "paper", "scissors"];
+  let result = "";
+
+  p_choice.innerHTML = `You chose: ${hands[p_hands]}`;
+  com_choice.innerHTML = `The computer chose: ${hands[com_hands]}`;
 
   if (com_hands === p_hands) {
-    return "Draw";
+    result = "Draw";
+    tie++;
   } else if (com_hands === 0 && p_hands === 1) {
-    return "You lose";
+    result = "You lose";
+    com_score++;
   } else if (com_hands === 1 && p_hands === 2) {
-    return "You lose";
+    result = "You lose";
+    com_score++;
   } else if (com_hands === 2 && p_hands === 0) {
-    return "You lose";
+    result = "You lose";
+    com_score++;
   } else {
-    return "You win";
+    result = "You win";
+    p_score++;
   }
+
+  result_text.innerHTML = `Result: ${result}`;
+  score.innerHTML = `Player: ${p_score} | Computer: ${com_score} | Ties: ${tie}`;
 }
